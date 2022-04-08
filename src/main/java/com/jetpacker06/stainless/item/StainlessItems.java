470c89bf06fb3b031ad3cd512a315062b5a927c8
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-04-08 12:29:08 -0500

    spaces

--- src/main/java/com/jetpacker06/stainless/item/StainlessItems.java
+++ src/main/java/com/jetpacker06/stainless/item/StainlessItems.java
@@ -10,8 +10,8 @@ import net.minecraftforge.registries.RegistryObject;
 public class StainlessItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Stainless.MOD_ID);
     //begin items
-    public static final RegistryObject<Item> STEEL_INGOT =ITEMS.register("steel_ingot", () -> new Item(new Item.Properties().tab(ItemGroups.STAINLESS)));
-    public static final RegistryObject<Item> STEEL_NUGGET =ITEMS.register("steel_nugget", () -> new Item(new Item.Properties().tab(ItemGroups.STAINLESS)));
+    public static final RegistryObject<Item> STEEL_INGOT = ITEMS.register("steel_ingot", () -> new Item(new Item.Properties().tab(ItemGroups.STAINLESS)));
+    public static final RegistryObject<Item> STEEL_NUGGET = ITEMS.register("steel_nugget", () -> new Item(new Item.Properties().tab(ItemGroups.STAINLESS)));
     //end items
     public static void register(IEventBus eventBus) {ITEMS.register(eventBus);}
 }
