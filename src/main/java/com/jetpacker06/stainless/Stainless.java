f4c415563bad1ba530961ff5fcfc695ae9c7752f
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-04-08 09:10:28 -0500

    Branch rename

--- src/main/java/com/jetpacker06/stainless/Stainless.java
+++ src/main/java/com/jetpacker06/stainless/Stainless.java
@@ -1,19 +1,15 @@
 package com.jetpacker06.stainless;
 
 import com.jetpacker06.stainless.item.StainlessItems;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("stainless")
 public class Stainless {
     public static final String MOD_ID = "stainless";
@@ -28,16 +24,7 @@ public class Stainless {
 
     private void setup(final FMLCommonSetupEvent event)
     {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("HELLO FROM PRE INIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
-
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
     }
 }
