bba80788bbab6da40aa8ec7a3d35453a104661d0
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-06-20 09:41:08 -0500

    server patch

--- src/main/java/com/jetpacker06/stainless/Stainless.java
+++ src/main/java/com/jetpacker06/stainless/Stainless.java
@@ -1,7 +1,7 @@
 package com.jetpacker06.stainless;
 
-import com.jetpacker06.stainless.block.StainlessBlockEntities;
 import com.jetpacker06.stainless.block.AllBlocks;
+import com.jetpacker06.stainless.block.StainlessBlockEntities;
 import com.jetpacker06.stainless.item.AllItems;
 import com.jetpacker06.stainless.recipe.RecipeTypes;
 import com.jetpacker06.stainless.screen.AlloyBlasterScreen;
@@ -10,7 +10,7 @@ import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -30,7 +30,7 @@ public class Stainless {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
         MinecraftForge.EVENT_BUS.register(this);
     }
-    private void clientSetup(final FMLCommonSetupEvent event) {
+    private void clientSetup(final FMLClientSetupEvent event) {
         MenuScreens.register(MenuTypes.ALLOY_BLASTER_MENU.get(), AlloyBlasterScreen::new);
     }
 }
